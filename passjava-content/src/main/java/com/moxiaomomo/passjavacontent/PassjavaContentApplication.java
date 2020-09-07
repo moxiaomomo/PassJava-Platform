f68789693c1cac5fa033dcc36b80cab0eb906428
moguang <809444138@qq.com>
2020-09-07 17:20:55 +0800

    Add mybatis test logic

--- passjava-content/src/main/java/com/moxiaomomo/passjavacontent/PassjavaContentApplication.java
+++ passjava-content/src/main/java/com/moxiaomomo/passjavacontent/PassjavaContentApplication.java
@@ -2,7 +2,13 @@ package com.moxiaomomo.passjavacontent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+// import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+//@EnableDiscoveryClient
+// @ComponentScan(basePackages = {"com.moxiaomomo.passjavacontent.config"})
+@EnableSwagger2
 @SpringBootApplication
 public class PassjavaContentApplication {
 
